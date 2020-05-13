fa626f6b0e719f183127be486e6a6beb51b02c04
Brandon Perez <bmperez@alumni.cmu.edu>
2020-05-12 22:49:45 -0700

    Extended the FZF edit shortcut to accept paths as arguments for Bash.
    Previously, the FZF edit shortcut (Alt-e) did not accept any arguments
    and would thus always search from the current working directory. Now,
    the shortcut and the function that backs it accepts arguments. If a
    single argument is specified, then it is treated as the root path from
    which the search for files to edit is conducted. In order to be more
    usable, if more than one argument is specified then it is ignored. This
    is done so the user doesn't have to explicitly clear a very long
    command-line.
    
    The shortcut accomplishes accepting arguments by prefixing the current
    line with usual FZF edit function with command substitution and then
    ends the line with a parenthesis, so the current line is passed as
    arguments into the function.

--- bash/readline.bash
+++ bash/readline.bash
@@ -26,16 +26,30 @@
 # Use FZF to select a set of file(s) to edit, and then open the files with Vim (they will setup as separate buffers).
 function fzf-edit
 {
+    # If specified, search from the specified path. If more than one argument is specified, then the arguments are
+    # ignored (since the shortcut simply will pass in the whole line). Defaults to the current working directory.
+    if [[ ${#} -eq 1 ]]; then
+        local search_directory="${1}"
+    else
+        local search_directory="$(pwd)"
+    fi
+
+    # Verify the specified path is a directory.
+    if [[ ! -d "${search_directory}" ]]; then
+        printf "\nError: ${search_directory}: Specified path is not a directory.\n" 1>&2
+        return 1
+    fi
+
     # Define the command used to select the file(s) to edit. Note that the null character is used to delimit the outputs
     # so that all files with special characters can be handled properly.
-    local fzf_cmd='command fd --color never --follow --hidden --no-ignore --type file --print0 ${FD_TEXT_IGNORE} |
-        fzf ${FZF_DEFAULT_OPTS} --height "40%" --reverse --read0 --print0'
+    local fzf_cmd='command fd --color never --follow --hidden --no-ignore --type file --print0 ${FD_TEXT_IGNORE}
+            --search-path "${search_directory}" | fzf ${FZF_DEFAULT_OPTS} --height "40%" --reverse --read0 --print0'
 
-    # Use the map file primitive to put the select files into an array variable.
+    # Use the map file primitive to put the selected files into an array variable.
     local files=()
     IFS= mapfile -d $'\0' files < <(eval ${fzf_cmd})
 
-    # Only attempt to open files if some have been selected by the user.
+    # Only attempt to open files if some have been selected by the user. Otherwise, clear out the whole line of text.
     if [[ ${#files[@]} -ne 0 ]]; then
         printf 'vim ' &&
         printf '%q ' "${files[@]}"
@@ -58,5 +72,7 @@ bind 'set enable-bracketed-paste on'
 # Keyboard Shortcuts and Bindings
 #-----------------------------------------------------------------------------------------------------------------------
 
-# Setup ALT-e as a shortcut to edit a file. The file is searched for with FZF and then opened up with Vim.
-bind '"\ee": "\C-e\C-u$(fzf-edit)\e\C-e\er\C-m"'
+# Setup ALT-e as a shortcut to edit a file. The file is searched for with FZF and then opened up with Vim. If the line
+# is not blank, then it is passed along to the function. This allows for paths to be specified where the search should
+# be rooted.
+bind '"\ee": "\C-a$(fzf-edit \C-e)\e\C-e\er\C-m"'
